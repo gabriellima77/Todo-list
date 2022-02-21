e1dab209b05852d9c2d04e80b99f892be2f51c15
Gabriel Lima <gabrielpk1@live.com>
2022-02-21 15:17:57 -0300

    started server and add the task router

---
+++ api/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+
+app.use(express.json());
+app.listen(port, () => console.log('Server On!'));
---
+++ api/routers/tasks/index.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+
+router.get('/', (_, res)=> {
+
+});
+
+router.post('/', (_, res)=> {
+
+});
+
+router.delete('/', (_, res)=> {
+
+});
+
+router.put('/', (_, res)=> {
+
+});
